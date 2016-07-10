1b839be75b3b6b6b06fb666b2d7a21a7a48d380e
Angela Reid <reidang94@gmail.com>
2016-07-09 21:15:17 -0400

    added imgs modified html and js on page

--- assets/script.js
+++ assets/script.js
@@ -13,13 +13,13 @@ function updateMessage() {
 updateMessage();
 
 
-//Get Random number function
+//Get Random number function and button to refresh page
 function getRandomNum() {
  	var x = 0;
 	var r = document.getElementById('info');
 	var randomNum = Math.floor((Math.random() * 10) + 1);
 
-	r.innerHTML = '<h1>Random Number</h1><h2>' + randomNum + '</h2>';
+	r.innerHTML = '<h1>Random Number</h1><p><INPUT Type="button" VALUE="Get Number" onClick="history.go(0)"></p><h2>' + randomNum + '</h2>';	
 }
 
 // Call the function
@@ -27,9 +27,9 @@ getRandomNum();
 
 
 //get Saying length 
-var saying = "Home Sweet Home";
+var saying = '"Coding is Fun!"';
 
-var message = "<h2>Saying length</h2><p>" + saying.length + "</p>";
+var message = "<h2>JavaScript</h2><p>" + "<br>" + saying + "<br>" + "Saying length: " + saying.length + "</p>";
 
 var element = document.getElementById("messageInABottle").innerHTML = message;
 
