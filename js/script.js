b62fef5f409d23434d9c18a700aebcf0e0fc2045
Angela Reid <reidang94@gmail.com>
2016-07-13 17:05:39 -0400

    fixed conflicts

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var msg1 = "To be confident as a Developer";
+var msg2 = "To really understand the concepts";
+var msg3 = "To have fun learning";
+
+
+function updateMessage() {
+
+	var el1 = document.getElementById('message1').innerHTML = msg1;
+	var el2 = document.getElementById('message2').innerHTML = msg2;
+	var el3 = document.getElementById('message3').innerHTML = msg3;
+}
+
+updateMessage();
+
+
+//Get Random number function and button to refresh page
+function getRandomNum() {
+ 	var x = 0;
+	var r = document.getElementById('info');
+	var randomNum = Math.floor((Math.random() * 10) + 1);
+
+	r.innerHTML = '<h2>JS Random Number</h2><p><INPUT Type="button" VALUE="Get Number" onClick="history.go(0)"></p><h2>' + randomNum + '</h2>';	
+}
+
+// Call the function
+getRandomNum();
+
+
+//get Saying length 
+var saying = '"Coding is Fun!"';
+
+var message = "<h3>JavaScript</h3><p>" + "<br>" + saying + "<br>" + "Saying length: " + saying.length + "</p>";
+
+var element = document.getElementById("messageInABottle").innerHTML = message;
+
